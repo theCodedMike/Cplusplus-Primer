978b0fe08da719b62d2c0bff95414fb5b0ba36cb
theCodedMike <lixiaofeng1115@outlook.com>
2025-10-10 17:37:11 +0800

    19.8 nonportable

--- chapter_19/listing_spec_tools_and_techs.cpp
+++ chapter_19/listing_spec_tools_and_techs.cpp
@@ -3,6 +3,8 @@
 //
 #include <iostream>
 #include <windows.h>
+
+#include "../include/Screen.h"
 using namespace std;
 
 class Base {
@@ -60,12 +62,14 @@ private:
 void test_oop();
 void typeid_cmp();
 void use_rtti();
+void use_enum();
 
 int main(int argc, char *argv[]) {
     SetConsoleOutputCP(CP_UTF8);
     //test_oop();
     //typeid_cmp();
-    use_rtti();
+    //use_rtti();
+    use_enum();
 }
 
 void test_oop() {
@@ -127,4 +131,115 @@ void use_rtti() {
          << typeid(Derived).name() << endl     // 7Derived
          << typeid(p).name() << endl           // P4Base
          << typeid(*p).name() << endl;         // 7Derived
+}
+
+void use_enum() {
+    // 限定作用域的枚举类型 scoped enumeration
+    enum class open_modes {
+        input, output, append
+    };
+    // 不限定作用域的枚举类型 unscoped enumeration
+    enum color {
+        red, yellow, green
+    };
+    enum class int_types {
+        char_type = 8, short_type = 16, int_type = 16, long_type = 32, long_long_type = 64
+    };
+
+    constexpr int i = color::yellow; // ✅
+    //int j = int_types::short_type; // ❌ 限定作用域的枚举类型不能进行隐式转换
+    cout << red <<" " << i << " " << green << endl; // 0 1 2
+
+    // 指定enum的大小
+    enum intValues : unsigned long long { // 默认是int类型
+        // ...
+    };
+}
+
+void ptr_to_mem() {
+    // pointer to member
+    //auto pdata = &Screen::contents;
+    //const string Screen::*pdata = &Screen::contents; // 指向类成员变量的指针
+    //auto pmf = &Screen::display; // 成员函数指针
+    //Screen s;
+    //s.*pdata;
+    //(s.*pmf)(cout)
+}
+
+void nested_class() {
+    class TextQuery {
+    public:
+        class QueryResult; // 稍后定义
+    };
+    class TextQuery::QueryResult {
+        // ....
+    };
+}
+
+void use_union() {
+    // Token类型的对象只有一个成员，该成员的类型可能是下列类型中的任意一种
+    union Token { // 默认是public
+        char cval;
+        int ival;
+        double dval;
+    };
+    Token first = { 'a' };
+    Token last;
+    Token *pt = new Token;
+    last.cval = 'z';
+    pt->ival = 42;
+
+    delete pt;
+}
+
+void bit_field() {
+    typedef unsigned int Bit;
+    class File {
+        Bit mode: 2; // mode占2位
+        Bit modified: 1;
+        Bit prot_owner: 3;
+        Bit prot_group: 3;
+        Bit prot_world: 3;
+
+    public:
+        enum modes {
+            READ = 01, WRITE = 02, EXECUTE = 03
+        };
+        File & open(modes) {
+            mode |= READ;
+            if (mode & WRITE)
+                ;//...
+            return *this;
+        }
+        void close() {
+            if (modified)
+                ; // save
+        }
+        void write() {
+            modified = 1;
+            // ...
+        }
+        bool is_read() const {
+            return mode & READ;
+        }
+        void set_write() {
+            mode |= WRITE;
+        }
+    };
+}
+
+void use_volatile() {
+    volatile int display_register; // 该int值可能发生改变
+    volatile int iax[10]; // iax的每个元素都是volatile
+    volatile Screen s; // s的每个成员都是volatile
+}
+
+void linkage_directive() {
+    /*
+    // cstring
+    extern "C" size_t strlen(const char *);
+    extern "C" {
+        int strcmp(const char *, const char *);
+        char *strcat(char *, const char *);
+    }*/
 }
