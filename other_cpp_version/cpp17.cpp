9b012a291dc2e2346ae95fe3998afe6ed3a4b1ab
theCodedMike <lixiaofeng1115@outlook.com>
2025-10-10 21:22:11 +0800

    补充C++其他版本

---
+++ other_cpp_version/cpp17.cpp
@@ -0,0 +1,7 @@
+//
+// Created by lixia on 2025/10/10.
+//
+
+int main(int argc, char *argv[]) {
+
+}
---
+++ other_cpp_version/cpp20.cpp
@@ -0,0 +1,8 @@
+//
+// Created by lixia on 2025/10/10.
+//
+
+
+int main(int argc, char *argv[]) {
+
+}
